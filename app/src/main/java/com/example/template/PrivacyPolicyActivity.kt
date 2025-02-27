1e2c76e6072a1c05eee28dcc546045347c5c0087
MahmoudNabilApps <mahmoudnabilapps@gmail.com>
2025-02-27 10:55:21 +0200

    Create PrivacyPolicyActivity.kt

---
+++ app/src/main/java/com/example/template/PrivacyPolicyActivity.kt
@@ -0,0 +1,18 @@
+package com.example.template
+
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class PrivacyPolicyActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_privacy_policy)
+
+        val webView: WebView = findViewById(R.id.webViewPrivacy)
+        webView.webViewClient = WebViewClient()
+        webView.settings.javaScriptEnabled = true
+        webView.loadUrl(getString(R.string.privacy_url))
+    }
+}
